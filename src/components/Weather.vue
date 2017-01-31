38eeacc2834dbfa3a3919b8d7b79efeda3702ff7
David Martinez <davidmgilo@gmail.com>
2017-01-31 16:13:28 +0100

    refactored

---
+++ src/components/Weather.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="weather">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weather',
+  data () {
+    return {
+      msg: 'Aquí weather todo'
+    }
+  }
+}
+</script>
+
+</style>

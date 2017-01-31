cbded472d53bfb555feeb5fa9d2add0346f9b0c1
David Martinez <davidmgilo@gmail.com>
2017-01-31 16:23:12 +0100

    some content

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,7 +3,21 @@
     <div class="location">{{location}}</div>
     <div class="date">{{date}}</div>
     <div class="description">{{description}}</div>
+    <div class="current">
+      <div class="visual">
+        <div class="cloud">cloud</div>
+        <div class="temp">{{temp}}</div>
+        <div class="scale">º</div>
+      </div>
+      <div class="text">
+        <div class="precipitation">Precipitation: {{precipitation}}</div>
+        <div class="humidity">Humidity: {{humidity}}</div>
+        <div class="wind">Wind: {{wind}}</div>
+        <div class="pollen">Pollen: {{pollen}}</div>
+      </div>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -13,7 +27,12 @@ export default {
     return {
       location: 'New York, NY',
       date: 'Tuesday, April 15th',
-      description: 'desc here'
+      description: 'desc here',
+      temp: 25,
+      precipitation: '100%',
+      humidity: '97%',
+      wind: '4mph SW',
+      pollen: 36
     }
   }
 }
